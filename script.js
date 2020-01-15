18fd920d7f4a4f3e4229f1c4871af59b7d7c29a0
Neha Gupta <er.nehaa.gupta@gmail.com>
2020-01-14 19:15:09 -0500

    Achievement Page complete

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+  /*Scroll to top when arrow up clicked BEGIN*/
+  $(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+  });
 $(document).ready(function(){
     $('#myCarousel').carousel({
       interval: 4000
@@ -21,6 +30,15 @@ $(document).ready(function(){
     FB.XFBML.parse();
     }
   }; 
+
+  $("#back2Top").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+  });
+  /*Scroll to top when arrow up clicked END*/
+
+
 });
 
   
